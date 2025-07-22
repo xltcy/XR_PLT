e5db7ebf963146eaab57a7a4fa12d46d852ec36d
cynLogin <144438294+cynLogin@users.noreply.github.com>
2025-07-22 17:18:21 +0800

    add outline plugin and complete highlight

--- Assets/Scripts/UI/HighLight.cs
+++ Assets/Scripts/UI/HighLight.cs
@@ -9,35 +9,25 @@ public class HighLight : MonoBehaviour
     public Transform boardMiddle;
     public Transform sonarUnderneath;
 
-    private List<Material> sonarAboveMats = new List<Material>();
-    private List<Material> boardMiddleMats = new List<Material>();
-    private List<Material> sonarUnderneathMats = new List<Material>();
-
-    private List<Color> sonarAboveOriginal = new List<Color>();
-    private List<Color> boardMiddleOriginal = new List<Color>();
-    private List<Color> sonarUnderneathOriginal = new List<Color>();
 
     void Start()
     {
-        InitGroup(sonarAbove, sonarAboveMats, sonarAboveOriginal);
-        InitGroup(boardMiddle, boardMiddleMats, boardMiddleOriginal);
-        InitGroup(sonarUnderneath, sonarUnderneathMats, sonarUnderneathOriginal);
+        InitGroup(sonarAbove);
+        InitGroup(boardMiddle);
+        InitGroup(sonarUnderneath);
     }
 
-    void InitGroup(Transform groupRoot, List<Material> matList, List<Color> colorList)
+    void InitGroup(Transform groupRoot)
     {
+        GameObject grobj = groupRoot.gameObject;
         if (groupRoot == null) return;
 
-        Renderer[] renderers = groupRoot.GetComponentsInChildren<Renderer>();
-
-        foreach (var r in renderers)
-        {
-            foreach (var mat in r.materials)
-            {
-                matList.Add(mat);
-                colorList.Add(mat.GetColor("_EmissionColor"));
-            }
-        }
+        grobj.AddComponent<Outline>();
+        Outline otl = grobj.GetComponent<Outline>();
+        otl.OutlineMode = Outline.Mode.OutlineAll;
+        otl.OutlineColor = highlightcolor;
+        otl.OutlineWidth = 10f;
+        otl.enabled = false;
     }
 
     /*void SetGroupEmission(List<Material> matList, Color color)
@@ -48,16 +38,12 @@ public class HighLight : MonoBehaviour
             mat.SetColor("_EmissionColor", color);
         }
     }*/
-    void SetGroupEmission(Transform show, Transform hide1,Transform hide2 , List<Material> matList, Color color)
+    void SetGroupEmission(Transform show, Transform hide1,Transform hide2)
     {
         show.gameObject.SetActive(true);
         hide1.gameObject.SetActive(false);
         hide2.gameObject.SetActive(false);
-        foreach (var mat in matList)
-        {
-            mat.EnableKeyword("_EMISSION");
-            mat.SetColor("_EmissionColor", color);
-        }
+        show.gameObject.GetComponent<Outline>().enabled = true;
     }
     /*void ResetGroupEmission(List<Material> matList, List<Color> original)
     {
@@ -66,30 +52,27 @@ public class HighLight : MonoBehaviour
             matList[i].SetColor("_EmissionColor", original[i]);
         }
     }*/
-    void ResetGroupEmission(Transform show, Transform hide1, Transform hide2, List<Material> matList, List<Color> original)
+    void ResetGroupEmission(Transform show, Transform hide1, Transform hide2)
     {
         show.gameObject.SetActive(true);
         hide1.gameObject.SetActive(true);
         hide2.gameObject.SetActive(true);
-        for (int i = 0; i < matList.Count; i++)
-        {
-            matList[i].SetColor("_EmissionColor", original[i]);
-        }
+        show.gameObject.GetComponent<Outline>().enabled = false;
     }
     //以下是高亮函数（仅显示某部分）
     //public void HighlightAbove() => SetGroupEmission(sonarAboveMats, highlightcolor);
-    public void HighlightAbove() => SetGroupEmission(sonarAbove, boardMiddle, sonarUnderneath, sonarAboveMats, highlightcolor);
+    public void HighlightAbove() => SetGroupEmission(sonarAbove, boardMiddle, sonarUnderneath);
     //public void HighlightMiddle() => SetGroupEmission(boardMiddleMats, highlightcolor);
-    public void HighlightMiddle() => SetGroupEmission(boardMiddle, sonarAbove, sonarUnderneath, boardMiddleMats, highlightcolor);
+    public void HighlightMiddle() => SetGroupEmission(boardMiddle, sonarAbove, sonarUnderneath);
     //public void HighlightUnderneath() => SetGroupEmission(sonarUnderneathMats, highlightcolor);
-    public void HighlightUnderneath() => SetGroupEmission(sonarUnderneath, sonarAbove, boardMiddle, sonarUnderneathMats, highlightcolor);
+    public void HighlightUnderneath() => SetGroupEmission(sonarUnderneath, sonarAbove, boardMiddle);
 
 
     //以下是恢复原状态函数（三个部分全部显示）
     //public void ResetAbove() => ResetGroupEmission(sonarAboveMats, sonarAboveOriginal);
-    public void ResetAbove() => ResetGroupEmission(sonarAbove, boardMiddle, sonarUnderneath, sonarAboveMats, sonarAboveOriginal);
+    public void ResetAbove() => ResetGroupEmission(sonarAbove, boardMiddle, sonarUnderneath);
     //public void ResetMiddle() => ResetGroupEmission(boardMiddleMats, boardMiddleOriginal);
-    public void ResetMiddle() => ResetGroupEmission(boardMiddle, sonarAbove, sonarUnderneath, boardMiddleMats, boardMiddleOriginal);
+    public void ResetMiddle() => ResetGroupEmission(boardMiddle, sonarAbove, sonarUnderneath);
     //public void ResetUnderneath() => ResetGroupEmission(sonarUnderneathMats, sonarUnderneathOriginal);
-    public void ResetUnderneath() => ResetGroupEmission(sonarUnderneath, sonarAbove, boardMiddle, sonarUnderneathMats, sonarUnderneathOriginal);
+    public void ResetUnderneath() => ResetGroupEmission(sonarUnderneath, sonarAbove, boardMiddle);
 }
