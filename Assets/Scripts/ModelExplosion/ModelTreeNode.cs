17124db808179546607042727484d7cca229b92e
tcluan <tcluan@buaa.edu.cn>
2025-12-08 09:47:12 +0800

    [ARView][太和殿] 添加太和殿模型，添加太和殿爆炸图效果

--- Assets/Scripts/ModelExplosion/ModelTreeNode.cs
+++ Assets/Scripts/ModelExplosion/ModelTreeNode.cs
@@ -20,10 +20,10 @@ public class ModelTreeNode : MonoBehaviour
     [Header("Child")]
     public List<GameObject> _children;
     [Header("Property")]
-    public float _mass;
+    public float _mass=1.0f;
     public Vector3 _standardAxis;
-    public float _standardIntensity;    // 每个子节点爆炸时移动的距离
-    public float _factor;       // 当前节点爆炸时移动距离的权重
+    public float _standardIntensity=1.0f;    // 每个子节点爆炸时移动的距离
+    public float _factor=1.0f;       // 当前节点爆炸时移动距离的权重
     #endregion
 
     #region PrivateVar
@@ -122,7 +122,7 @@ public class ModelTreeNode : MonoBehaviour
         // 如果当前节点是叶子节点，则模型的中心即为当前节点的中心
         if (_isLeafNode == true)
         {
-            // Debug.Log("[tcluan Debug] " + name + " calcualates center.\n");
+             //Debug.Log("[tcluan Debug] " + name + " calcualates center.\n");
             _center = _children[0].GetComponent<ModelComponent>().CalculateCenter();
         }
         // 否则，当前节点为内部节点，则模型的中心为其各个子节点的中心
@@ -139,7 +139,7 @@ public class ModelTreeNode : MonoBehaviour
             }
             _center /= totalMass;
         }
-        // Debug.Log("[tcluan Debug] " + name + " center: " + _center);
+         //Debug.Log("[tcluan Debug] " + name + " center: " + _center);
     }
 
     /// <summary>
@@ -179,7 +179,7 @@ public class ModelTreeNode : MonoBehaviour
         _direction = direction;
         _deltaIntensity = intensity * _factor * _deltaTime;
         _time += 1.0f;
-        CenterMovement(direction, intensity);
+        CenterMovement(direction, _factor * intensity);
     }
 
     /// <summary>
@@ -189,7 +189,7 @@ public class ModelTreeNode : MonoBehaviour
     /// <param name="intensity">移动距离</param>
     private void CenterMovement(Vector3 direction, float intensity)
     {
-        _center += direction * _factor * intensity;
+        _center += direction * intensity;
         if (_isLeafNode == true)
         {
             return;
