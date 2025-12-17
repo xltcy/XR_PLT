02a69e7d5ba1d9df463606b270951ff7c438f83d
xltcy <2036376334@qq.com>
2025-12-17 17:04:27 +0800

    [ARView][GXL]更换虚拟人模型和语音

--- Assets/Scripts/Talker/AzureAuth.cs
+++ Assets/Scripts/Talker/AzureAuth.cs
@@ -1,18 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Microsoft.CognitiveServices.Speech;
+
+//static class AzureAuth
+//{
+//    public static readonly SpeechConfig SpeechConfig = SpeechConfig.FromSubscription("92558acf9b4343989b766a852130b139", "eastasia");
+//    static AzureAuth()
+//    {
+//        //SpeechConfig.SpeechRecognitionLanguage = Config.Configs.Language;
+//        //SpeechConfig.SpeechSynthesisLanguage = Config.Configs.Language;
+//        SpeechConfig.SpeechRecognitionLanguage = Config.Configs.Language;
+//        SpeechConfig.SpeechSynthesisLanguage = Config.Configs.Language;
+//    }
+//}
+
 using Microsoft.CognitiveServices.Speech;
 
-static class AzureAuth
+public static class AzureAuth
 {
-    public static readonly SpeechConfig SpeechConfig = SpeechConfig.FromSubscription("92558acf9b4343989b766a852130b139", "eastasia");
-    static AzureAuth()
+    private static SpeechConfig _speechConfig;
+
+    public static SpeechConfig SpeechConfig
     {
-        //SpeechConfig.SpeechRecognitionLanguage = Config.Configs.Language;
-        //SpeechConfig.SpeechSynthesisLanguage = Config.Configs.Language;
-        SpeechConfig.SpeechRecognitionLanguage = Config.Configs.Language;
-        SpeechConfig.SpeechSynthesisLanguage = Config.Configs.Language;
+        get
+        {
+            if (_speechConfig == null)
+            {
+                _speechConfig = SpeechConfig.FromSubscription("92558acf9b4343989b766a852130b139", "eastasia");
+                //_speechConfig.SpeechSynthesisVoiceName = "zh-CN-XiaoxiaoNeural";
+                _speechConfig.SpeechSynthesisVoiceName = "zh-CN-YunfengNeural";
+                _speechConfig.SetSpeechSynthesisOutputFormat(SpeechSynthesisOutputFormat.Riff24Khz16BitMonoPcm);
+            }
+            return _speechConfig;
+        }
     }
 }
